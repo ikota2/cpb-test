dfdfc1e22cd065d7f4b865aca30d0436e694894e
Ivan Kotov <polyzay@gmail.com>
2024-02-01 00:36:09 +0100

    cors installed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const cors = require('cors');
 const fetchProducts = require('./fetchProducts');
 
 const schema = buildSchema(`
@@ -21,6 +22,7 @@ const root = {
 };
 
 const app = express();
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
