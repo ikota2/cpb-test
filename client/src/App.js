510800571e92ce62518c5be290b95b5c2fd4ae22
Ivan Kotov <polyzay@gmail.com>
2024-02-01 00:37:06 +0100

    fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { useEffect } from "react";
 
 function App() {
+    function fetchProducts() {
+        fetch('http://localhost:3000/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `{
+                      getProducts {
+                          id, title
+                      }
+                }`
+            })
+        })
+            .then(res => res.json())
+            .then(res => console.log(res.data));
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+
   return (
     <div className="App">
-
+      hello react
     </div>
   );
 }
