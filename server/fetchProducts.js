854864cad976025528f56ae8623e6e34142179ed
Ivan Kotov <polyzay@gmail.com>
2024-01-31 20:50:26 +0100

    save to cache

--- server/fetchProducts.js
+++ server/fetchProducts.js
@@ -1,31 +1,45 @@
+const fetch = require('node-fetch');
+const NodeCache = require('node-cache');
 require('dotenv').config();
 const { SHOPIFY_STORE_URL, SHOPIFY_ACCESS_TOKEN } = process.env;
-const fetch = require('node-fetch');
 
+const cache = new NodeCache({ stdTTL: 7200 });
 const graphqlEndpoint = `https://${SHOPIFY_STORE_URL}/admin/api/2023-01/graphql.json`;
 
-const query = `{
-  products (first: 3) {
-    edges {
-      node {
-        id
-        title
-      }
-    }
-  }
-}`;
+const fetchProducts = async () => {
+    const cacheKey = 'products';
+    const cachedData = cache.get(cacheKey);
+    if (cachedData) return cachedData;
 
-fetch(graphqlEndpoint, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': SHOPIFY_ACCESS_TOKEN,
-    },
-    body: JSON.stringify({ query }),
-})
-    .then(response => response.json())
-    .then(data => {
-        // TODO cache
-        console.log(JSON.stringify(data, null, 2));
-    })
-    .catch(error => console.error('Error fetching products:', error));
+    const query = `{
+        products(first: 3) {
+            edges {
+                node {
+                    id
+                    title
+                }
+            }
+        }
+    }`;
+
+    try {
+        const response = await fetch(graphqlEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Shopify-Access-Token': SHOPIFY_ACCESS_TOKEN,
+            },
+            body: JSON.stringify({ query }),
+        });
+        const data = await response.json();
+        const products = data.data.products.edges.map(edge => edge.node);
+
+        cache.set(cacheKey, products);
+        return products;
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        throw error;
+    }
+};
+
+module.exports = fetchProducts;
--- server/index.js
+++ server/index.js
@@ -1,62 +1,34 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
-const fetch = require('node-fetch');
-require('dotenv').config();
-const { SHOPIFY_STORE_URL, SHOPIFY_ACCESS_TOKEN } = process.env;
+const fetchProducts = require('./fetchProducts');
 
 const schema = buildSchema(`
-  type Product {
-    id: ID!
-    title: String
-  }
+    type Product {
+        id: ID!
+        title: String
+    }
 
-  type Query {
-    getProducts: [Product]
-  }
+    type Query {
+        getProducts: [Product]
+    }
 `);
 
-const graphqlEndpoint = `https://${SHOPIFY_STORE_URL}/admin/api/2023-01/graphql.json`;
 const root = {
     getProducts: async () => {
-        const query = `{
-          products(first: 3) {
-            edges {
-              node {
-                id
-                title
-              }
-            }
-          }
-        }`;
-
-        try {
-            const response = await fetch(graphqlEndpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Shopify-Access-Token': SHOPIFY_ACCESS_TOKEN,
-                },
-                body: JSON.stringify({ query }),
-            });
-            const data = await response.json();
-            console.log('appdata ', data);
-            return data.data.products.edges.map(edge => edge.node);
-        } catch (error) {
-            console.error('Error fetching products:', error);
-            throw error;
-        }
+        return await fetchProducts();
     }
 };
 
-const index = express();
-index.use('/graphql', graphqlHTTP({
-    schema: schema,
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
     rootValue: root,
     graphiql: true,
 }));
 
 const port = 3000;
-index.listen(port, () => {
+app.listen(port, () => {
     console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
 });
